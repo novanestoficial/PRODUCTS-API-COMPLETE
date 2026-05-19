398ed9af76c9260e97357d9653106a3b4f01f23d
David Nest <davidcamposoliveira80@gmail.com>
2026-05-19 11:30:04 -0300

    Configuration class ProductController, add Complete CRUD

--- src/main/java/io/github/davidnest/productsgerence/products/Controller/ProductController.java
+++ src/main/java/io/github/davidnest/productsgerence/products/Controller/ProductController.java
@@ -2,16 +2,53 @@ package io.github.davidnest.productsgerence.products.Controller;
 
 
 import io.github.davidnest.productsgerence.products.Entity.Product;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.davidnest.productsgerence.products.Service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-    @PostMapping
-    public Product save(Product product) {
+    private final ProductService productService;
 
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+    
+    @PostMapping
+    public Product save(@RequestBody  Product product) {
+        return productService.saveProduct(product);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
+        return productService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        return ResponseEntity.ok(productService.findAllProducts());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+        return ResponseEntity.ok(productService.updateProduct(id, product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteByIds(@RequestBody List<Long> ids) {
+        productService.deleteProductsByIds(ids);
+        return ResponseEntity.noContent().build();
     }
 }
